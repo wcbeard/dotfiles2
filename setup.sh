380cb96d9712a405a6a8a2fdcd48a6e94f15ab64
d10genes <wcbeard10@gmail.com>
2017-07-14 15:21:06 -0400

    conditional jupyter

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
-mkdir -p .jupyter/custom
 
-cp custom.js ~/.jupyter/custom/
+
+if hash jupyter 2>/dev/null; then
+    JUPYTER=$(jupyter --data-dir)
+else
+    JUPYTER=~/.jupyter
+fi
+
+mkdir -p "$JUPYTER/custom"
+
+cp custom.js "$JUPYTER/custom/"
