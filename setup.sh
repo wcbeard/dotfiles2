0299c76ff6ac162a06cffffb832ff335d4acb806
d10genes <wcbeard10@gmail.com>
2017-07-20 19:24:21 -0400

    copy plugin

--- setup.sh
+++ setup.sh
@@ -11,5 +11,8 @@ mkdir -p "$JUPYTER/custom"
 cp custom.js "$JUPYTER/custom/"
 
 
+# Foldingtext needs to be installed
+cp -r done_cmd.ftplugin "~/Library/Application Support/FoldingText/Plug-Ins"
+
 # Brew
-# brew tap caskroom/fonts
+#
