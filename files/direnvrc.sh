0a61c72a7699b135f44a5304a2ab4a031ef868cd
wcbeard <wcbeard10@gmail.com>
2020-09-12 15:50:15 -0400

    direnvrc / conda hack

--- files/direnvrc.sh
+++ files/direnvrc.sh
@@ -4,12 +4,13 @@ layout_anaconda() {
   if [ -n "$1" ]; then
     # Explicit environment name from layout command.
     local env_name="$1"
-    source $ACTIVATE ${env_name}
   elif (grep -q name: environment.yml); then
     # Detect environment name from `environment.yml` file in `.envrc` directory
-    source $ACTIVATE `grep name: environment.yml | sed -e 's/name: //' | cut -d "'" -f 2 | cut -d '"' -f 2`
+    local env_name=`grep name: environment.yml | sed -e 's/name: //' | cut -d "'" -f 2 | cut -d '"' -f 2`
   else
     (>&2 echo No environment specified);
     exit 1;
   fi;
+  source $ACTIVATE $env_name
+  export CUSTOM_PS1="($env_name) "
 }
